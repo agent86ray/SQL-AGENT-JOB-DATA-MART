993da67d467d03f9bff9fc6392f24e07d69b69dc
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-10-12 13:42:53 -0400

    Add view vACTIVE_JOBS.

---
+++ SQL-Agent-Data-Mart-v19/SQL-Agent-Data-Mart/dbo/Views/vACTIVE_JOBS.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vACTIVE_JOBS]
+AS
+	SELECT 
+		[REFRESH_KEY]
+	,	COALESCE(j.[name], CONVERT(NVARCHAR(128), a.[job_id])) AS [JobName]
+	,	[CURRENT_DURATION]
+	,	[EXECUTION_COUNT]
+	,	[AVERAGE_DURATION]
+	,	[ESTIMATED_COMPLETION]
+	FROM [dbo].[ACTIVE_JOBS] a
+	JOIN [dbo].[JOB_CURRENT] c 
+	ON c.[job_id] = a.[job_id]
+	JOIN [dbo].[sysjobs] j
+	ON j.[JOB_KEY] = c.[JOB_KEY]
+	WHERE [REFRESH_KEY] = (
+		SELECT 
+			MAX([REFRESH_KEY])
+		FROM [dbo].[ACTIVE_JOBS_REFRESH]
+	);
