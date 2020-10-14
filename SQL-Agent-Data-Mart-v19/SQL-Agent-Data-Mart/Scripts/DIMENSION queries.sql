062c713e474825bd3d1c46328a61227c0d51f1db
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-10-14 06:33:09 -0400

    Update scripts.

--- SQL-Agent-Data-Mart-v19/SQL-Agent-Data-Mart/Scripts/DIMENSION queries.sql
+++ SQL-Agent-Data-Mart-v19/SQL-Agent-Data-Mart/Scripts/DIMENSION queries.sql
@@ -4,26 +4,27 @@
 USE [SQL_AGENT_DATA_MART];
 GO
 
-
--- clear SQL AGENT DIMENSIONS
-TRUNCATE TABLE [dbo].[ETL_DIMENSION_LOG];
-
-TRUNCATE TABLE [dbo].[sysjobs];
-
-TRUNCATE TABLE [dbo].[sysjobsteps];
-
-TRUNCATE TABLE [dbo].[JOB_CURRENT];
-
-
 -- query SQL AGENT DIMENSIONS
 SELECT * FROM [dbo].[ETL_DIMENSION_LOG];
+
+
+-- Show the "current" jobs
+SELECT * FROM [dbo].[vJOB_CURRENT]
+ORDER BY [JOB_KEY];
+
+
+SELECT *
+FROM msdb.dbo.sysjobactivity
+WHERE ;
+
 
 SELECT * FROM [dbo].[sysjobs];
 
 SELECT * FROM [dbo].[sysjobsteps];
 
-SELECT * FROM [dbo].[JOB_CURRENT]
-ORDER BY [job_id], [JOB_KEY];
+SELECT TOP 1 *
+FROM [dbo].[sysjobs];
+
 
 
 
@@ -31,3 +32,5 @@ ORDER BY [job_id], [JOB_KEY];
 
 
 */
+
+
--- SQL-Agent-Data-Mart-v19/SQL-Agent-Data-Mart/Scripts/HISTORY queries.sql
+++ SQL-Agent-Data-Mart-v19/SQL-Agent-Data-Mart/Scripts/HISTORY queries.sql
@@ -4,35 +4,80 @@ USE [SQL_AGENT_DATA_MART];
 GO
 
 
--- clear HISTORY tables
-
-TRUNCATE TABLE [dbo].[ETL_HISTORY_LOG];
-
-TRUNCATE TABLE [dbo].[JOB_INSTANCE];
-
-TRUNCATE TABLE [dbo].[JOB_STEP_INSTANCE];
+-- queries for PROCESS SQL AGENT DATA MART HISTORY job
 
 
--- HISTORY queries
-
+-- View the log for today
+DECLARE @TODAY DATETIME = CONVERT(DATE, GETDATE());
 SELECT *
-FROM [dbo].[ETL_HISTORY_LOG];
+FROM [dbo].[ETL_HISTORY_LOG]
+WHERE [START_DATE] > @TODAY;
 
+
+-- View the rows extracted from msdb.dbo.sysjobhistory.
+-- Filter out the SQL AGENT DATA MART jobs. We don't want
+-- to see them.
 SELECT *
-FROM [staging].[sysjobhistory]
+FROM [staging].[vsysjobhistory] 
 ORDER BY [instance_id];
+GO
+
+
+-- Show JOB_INSTANCE and JOB_STEP_INSTANCE -----------------------------
+DECLARE 
+	@ETL_KEY			INT
+,	@BEGIN_INSTANCE_ID	INT
+,	@END_INSTANCE_ID	INT;
+
+EXEC [dbo].[GET_ETL_HISTORY_LOG]
+	@P_ETL_KEY = @ETL_KEY OUTPUT
+,	@P_BEGIN_INSTANCE_ID = @BEGIN_INSTANCE_ID OUTPUT
+,	@P_END_INSTANCE_ID = @END_INSTANCE_ID OUTPUT;
+
+SELECT j.*
+FROM [dbo].[JOB_INSTANCE] j
+WHERE j.[INSTANCE_ID] BETWEEN @BEGIN_INSTANCE_ID AND @END_INSTANCE_ID;
+
+SELECT s.*
+FROM [dbo].[JOB_INSTANCE] j
+JOIN [dbo].[JOB_STEP_INSTANCE] s
+ON s.[JOB_INSTANCE_ID] = j.[INSTANCE_ID]
+WHERE j.[INSTANCE_ID] BETWEEN @BEGIN_INSTANCE_ID AND @END_INSTANCE_ID;
+
+----------------------------------------------------------------------
+
+
+SELECT *
+FROM [dbo].[JOB_STEP_INSTANCE];
+
+
+
+
+SELECT *
+FROM [dbo].[vJOB_CURRENT];
+
+SELECT *
+FROM [dbo].[JOB_CURRENT] c
+JOIN [dbo].[sysjobs] j ON j.[JOB_KEY] = c.[JOB];
+
 
 SELECT *
 FROM [dbo].[JOB_INSTANCE]
 ORDER BY [JOB_KEY], [INSTANCE_ID];
 
 
+
 SELECT *
 FROM [dbo].[JOB_STEP_INSTANCE]
-ORDER BY [JOB_KEY], [INSTANCE_ID], [step_id];
+WHERE [JOB_INSTANCE_ID] BETWEEN 92 AND 101
+ORDER BY [JOB_KEY], [JOB_INSTANCE_ID], [step_id];
 
 SELECT *
 FROM  [dbo].[JOB_INSTANCE_ALL_STEPS_COMPLETED]
 
+
+SELECT *
+FROM [dbo].[JOB_STEP_DURATION] 
+ORDER BY [JOB_KEY]
 
 */
